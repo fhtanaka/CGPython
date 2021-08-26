56e665a5bfb2953b3637c0abafd2903794307b5d
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2021-08-26 13:15:27 +0900

    revert change in add_inputs

--- node.py
+++ node.py
@@ -13,7 +13,8 @@ class Node:
         self.inputs = [] # List of node_ids
     
     def add_inputs(self, node_ids: list) -> None:
-        self.inputs += node_ids
+        for ids in node_ids:
+            self.inputs.append(ids)
     
     def remove_inputs(self, ids_to_remove: list) -> None:
         self.inputs = [i for i in self.inputs if i not in ids_to_remove]
