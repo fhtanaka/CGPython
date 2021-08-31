b21231c53cf9a3aa7168ad7c32afd8108171ec52
k1 <fhktanaka@gmail.com>
2021-08-31 15:30:24 +0900

    fixed bug in ordering and added parameters to pop init

--- population.py
+++ population.py
@@ -1,5 +1,5 @@
 from graph import Graph
-from typing import List
+from typing import List, Optional
 
 
 class Population:
@@ -12,22 +12,38 @@ class Population:
         n_col: int, 
         levels_back: int,
         n_champions: int,
-        mutation_strategy,
+        mutation_strategy: str,
         generations:int,
         minimize_fitness: bool = False,
+        point_mut_qnt: int = 1,
+        prob_mut_chance: float = 0.2,
+        mutate_active_only: bool = True,
         ):
         
         self.n_champions = n_champions
         self.mutation_strategy = mutation_strategy
         self.gens = generations
         self.population_size = population_size
+
+        self.minimize_fitness = 1
+        if minimize_fitness:
+            self.minimize_fitness = -1
+        if mutation_strategy != "point" and  mutation_strategy != "prob":
+            raise NameError("Mutation strategy should be \"point\" or \"prob\" ")
+        self.mutation_strategy = mutation_strategy
+        self.point_mut_qnt = point_mut_qnt             
+        self.prob_mut_chance = prob_mut_chance
+        self.mutate_active = mutate_active_only
+
         self.indvs: List[Graph] = []
         for _ in range(population_size):
             indv = Graph(n_in, n_out, n_row, n_col, levels_back)
             self.indvs.append(indv)
     
     def iterate_one_plus_lambda(self):
-        self.indvs.sort(key=lambda x: (x.id, x.fitness))
+        # This order the indvs first by ID (lesser IDs first) and then by fitness
+        # Since these sorts are stable, the indvs at the end of the array are the champions
+        self.indvs.sort(key=lambda x: (x.fitness * self.minimize_fitness, x.id))
         champions = self.indvs[-1*self.n_champions:]
 
         new_population: List[Graph] = []
@@ -42,7 +58,10 @@ class Population:
             new_population.append(parent)
             for _ in range(children_per_parent):
                 indv = parent.clone_graph()
-                indv.point_mutation(1, True) # TODO: adjust this to different types of mutation
+                if self.mutation_strategy == "point":
+                    indv.point_mutation(self.point_mut_qnt, self.mutate_active)
+                elif self.mutation_strategy == "prob":
+                    indv.probabilistic_mutation(self.prob_mut_chance, self.mutate_active)
                 new_population.append(indv)
         
         self.indvs = new_population
